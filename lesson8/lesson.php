6f18c2788ce33f48530db6eac611fbd8194336df
Diana <diana@taskertools.com>
2022-12-21 19:01:10 +0200

    lesson8

---
+++ lesson8/lesson.php
@@ -0,0 +1,11 @@
+<?php
+
+$string = "As labai dziaugiuosi, kad tirpsta sniegas";
+
+var_dump(str_replace('dziaugiuosi', 'liudziu', $string));
+
+//$existuojanciosReikmes = ['dziaugiuosi', 'tirpsta'];
+//$naujosReiksmes = ['liudziu', 'sninga'];
+//
+//var_dump(str_replace($existuojanciosReikmes, $naujosReiksmes, $string));
+
